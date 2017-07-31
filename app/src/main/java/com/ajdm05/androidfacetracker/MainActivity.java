15f4963c26e7347d803551e0d68d076b3b0f6ef1
Angela Delgado <ajdm05@gmail.com>
2017-07-31 11:24:57 -0600

    Adding main activity

--- app/src/main/java/com/ajdm05/androidfacetracker/MainActivity.java
+++ app/src/main/java/com/ajdm05/androidfacetracker/MainActivity.java
@@ -1,13 +1,187 @@
 package com.ajdm05.androidfacetracker;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import com.ajdm05.androidfacetracker.Camera.CameraOverlay;
+import com.ajdm05.androidfacetracker.Camera.CameraPreview;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CameraSource mCameraSource = null;
+    private CameraPreview mPreview;
+    private CameraOverlay cameraOverlay;
+    private static final int RC_HANDLE_GMS = 9001;
+    private static final int RC_HANDLE_CAMERA_PERM = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPreview = (CameraPreview) findViewById(R.id.preview);
+        cameraOverlay = (CameraOverlay) findViewById(R.id.faceOverlay);
+        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            createCameraSource();
+        } else {
+            requestCameraPermission();
+        }
+    }
+
+    private void requestCameraPermission() {
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+    }
+
+    private void createCameraSource() {
+
+        Context context = getApplicationContext();
+        //Creating the face detector
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        detector.setProcessor(
+                new MultiProcessor.Builder<>(new MainActivity.GraphicFaceTrackerFactory())
+                        .build());
+
+        if (!detector.isOperational()) {
+
+        }
+
+        //setting the camera source and using the front camera by default
+        mCameraSource = new CameraSource.Builder(context, detector)
+                .setRequestedPreviewSize(640, 480)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedFps(30.0f)
+                .build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        startCameraSource();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPreview.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mCameraSource != null) {
+            mCameraSource.release();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode != RC_HANDLE_CAMERA_PERM) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            createCameraSource();
+            return;
+        }
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("AndroidFaceTrackerDemo")
+                .setMessage("Need Camera permission!")
+                .setPositiveButton("OK", listener)
+                .show();
+    }
+
+    private void startCameraSource() {
+        int code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(
+                getApplicationContext());
+        if (code != ConnectionResult.SUCCESS) {
+            Dialog dlg =
+                    GoogleApiAvailability.getInstance().getErrorDialog(this, code, RC_HANDLE_GMS);
+            dlg.show();
+        }
+
+        if (mCameraSource != null) {
+            try {
+                mPreview.start(mCameraSource, cameraOverlay);
+            } catch (IOException e) {
+                mCameraSource.release();
+                mCameraSource = null;
+            }
+        }
+    }
+
+    //Detector process that manage the results
+    private class GraphicFaceTrackerFactory implements MultiProcessor.Factory<Face> {
+        @Override
+        public Tracker<Face> create(Face face) {
+            return new MainActivity.GraphicFaceTracker(cameraOverlay);
+        }
+    }
+
+    private class GraphicFaceTracker extends Tracker<Face> {
+        private CameraOverlay mOverlay;
+        private FaceOverlay faceOverlayGraphics;
+
+        //Create a new FaceOverlay instance which manage the rectangle around the face tracked
+        GraphicFaceTracker(CameraOverlay overlay) {
+            mOverlay = overlay;
+            faceOverlayGraphics = new FaceOverlay(overlay);
+        }
+
+        @Override
+        public void onNewItem(int faceId, Face item) {
+        }
+
+        @Override
+        public void onUpdate(FaceDetector.Detections<Face> detectionResults, Face face) {
+            mOverlay.add(faceOverlayGraphics);
+            faceOverlayGraphics.updateFace(face);
+        }
+
+        @Override
+        public void onMissing(FaceDetector.Detections<Face> detectionResults) {
+            mOverlay.remove(faceOverlayGraphics);
+        }
+
+        @Override
+        public void onDone() {
+            mOverlay.remove(faceOverlayGraphics);
+        }
     }
 }
